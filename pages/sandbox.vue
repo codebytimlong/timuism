846240cd6067c6c80598014499b21c966f7e6ba9
Tim Long <timuism@me.com>
2026-09-17 22:14:23 -0400

    add new sandbox view

---
+++ pages/sandbox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div ref="el" @click="handleClick">
+    Hey this is a div
+  </div>
+</template>
+
+<script setup lang="ts">
+const el = ref(null)
+
+function handleClick(){
+  alert("hey")
+}
+</script>
